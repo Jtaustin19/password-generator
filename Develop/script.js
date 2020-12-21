b54562efd5ee29008c351f1263c71f7f941e7759
Josh <jtaustin19@icloud.com>
2020-12-20 20:11:44 -0600

    add user prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,22 @@
-// Assignment code here
+/* User prompts */
+var howlong = window.prompt("How long would you like your password to be?")
+while (howlong < 8 || howlong > 128) {
+    howlong = window.prompt("This generator will create passwords no shorter than 8 characters long and no longer than 128 characters. How long would you like your password to be?");
+};
+
+var lowercaseConfirm = window.confirm("Does your password contain any lowercase letters?");
+var uppercaseConfirm = window.confirm("Does your password contain any uppercase letters?");
+var numbersConfirm = window.confirm("Does your password contain any numerics?");
+var symbolsConfirm = window.confirm("Does your password contain any symbols/special characters?");
+
+while (!(lowercaseConfirm || uppercaseConfirm || numbersConfirm || symbolsConfirm)) {
+    window.alert("You must select at least one of each character type!");
+    lowercaseConfirm = window.confirm("Does your password contain any lowercase letters?");
+    uppercaseConfirm = window.confirm("Does your password contain any uppercase letters?");
+    numbersConfirm = window.confirm("Does your password contain any numerics?");
+    symbolsConfirm = window.confirm("Does your password contain any symbols/special characters?");
+};
+/* User prompts end */
 
 
 // Get references to the #generate element
